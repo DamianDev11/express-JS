cf28e8f854611a5d34cb3c4d3041f16a9f460c70
Damian <damianprakash07@gmail.com>
2023-08-26 12:46:14 +0530

    First app using express

---
+++ StaticAssets/2.3_blog-chef/blog-chef/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { join } from "path";
+const app = express();
+
+app.use("/assets", express.static(join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Blog Chef says Hello!</h1>");
+});
+
+app.get("/admin/login", (req, res) => {
+  res.sendFile(join(__dirname, "views", "login.html"));
+});
+app.post("/admin/login", (req, res) => {
+  res.send("Handle login here...");
+});
+
+app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"));
